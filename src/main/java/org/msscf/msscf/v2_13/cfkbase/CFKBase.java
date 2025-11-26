2b5ce4229d7698c378c88918a7d5763fc51d5996
Mark Sobkow <mark.sobkow@gmail.com>
2025-11-26 16:24:17 -0600

    Skeleton for new project.

---
+++ src/main/java/org/msscf/msscf/v2_13/cfkbase/CFKBase.java
@@ -0,0 +1,41 @@
+/*
+ *	Mark's Code Fractal 2.13 CFKBase - Code Fractal Knowledge Base
+ *
+ *	Copyright (C) 2016-2025 Mark Stephen Sobkow
+ *
+ *	This file is part of Mark's Code Fractal 2.13, providing the extensible
+ *	knowledge base for starting to customize Mark's Code Fractal with your own
+ *	rules and custom verbs.
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *	
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *	
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see &lt;https://www.gnu.org/licenses/&gt;.
+ *	
+ *	If you wish to modify and use this code without publishing your changes,
+ *	or integrate it with proprietary code, please contact Mark Stephen Sobkow
+ *	for a commercial license at mark.sobkow@gmail.com
+ */
+
+package org.msscf.msscf.v2_13.cfkbase;
+
+public class CFKBase {
+	public final static String LinkName = "CFKBase";
+	public final static String LinkVersion = "3.0";
+
+	public static String getLinkName() {
+		return( LinkName );
+	}
+
+	public static String getLinkVersion() {
+		return( LinkVersion );
+	}
+}
