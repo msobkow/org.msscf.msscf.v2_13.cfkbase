7d19dbfc081dd70c1acc670077bf89e7f04d6c38
Mark Sobkow <mark.sobkow@gmail.com>
2026-07-09 14:37:44 -0600

    2026-07-09 14:35 CST The next phase of updating the existing private object interfaces to be based on modified versions of the schemaprot interfaces has begun. Then comes the hairy issue of modifying the _implementations_ for those interfaces to follow the new specifications (they're close, but not quite.)

---
+++ src/main/resources/server-markhome-mcf-30-toolset-java/java/src/makepriv.ex
@@ -0,0 +1,23 @@
+1,$s/Protected/Private/g
+1,$s/Prot/Priv/g
+1,$s/a protected/a private/g
+1,$s/Private License/Public License/g
+1,$s/\$prot/$/g
+1,$s/ prot/ /g
+1,$s/Public/Protected/g
+1,$s/Pub/Prot/g
+1,$s/a public/a protected/g
+1,$s/\$pub/$prot/g
+1,$s/ pub/ prot/g
+1,$s/Protected License/Public License/g
+1,$s/privected/private/g
+1,$s/ProtSchemaName\$TableData/PubSchemaName$TableData/g
+1,$s/ProtSchemaName\$RoleInfo/PubSchemaName$RoleInfo/g
+1,$s/DefSchemaPrivateTables/DefSchemaTables/g
+1,$s/PrivBuff/Buff/g
+1,$s/protlic/public/g
+1,$s/\$Priv\$/$$/g
+1,$s/\$priv\$/$$/g
+w
+q
+
