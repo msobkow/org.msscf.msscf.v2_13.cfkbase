31f2a89dbf0d7228ec8aff006a210e1ced562d40
Mark Sobkow <mark.sobkow@gmail.com>
2026-08-16 20:58:19 -0600

    2026-08-16 21:00 CST This is the end of the night for me; the missing references to the keyhash subpackage have been resolved, and now I'm seeing conflicts between CFLibUuid6 in dbutil and keyhash; the version in dbutil will be refactored to DbUuid6 so they can be distinguished properly, and the type references in the knowledge base updated accordingly after I'm done with the refactoring. But not right now. I'm done for the day.

---
+++ src/main/resources/server-markhome-mcf-30-toolset-java/java/src/fixprottypes.ex
@@ -0,0 +1,6 @@
+1,$s:implIJavaAtomType:implIProtJavaAtomType:g
+1,$s:implIJavaOptAtomType:implIProtJavaOptAtomType:g
+1,$s:implIJavaReqAtomType:implIProtJavaReqAtomType:g
+w
+q
+
---
+++ src/main/resources/server-markhome-mcf-30-toolset-java/java/src/fixpubtypes.ex
@@ -0,0 +1,6 @@
+1,$s:implIJavaAtomType:implIPubJavaAtomType:g
+1,$s:implIJavaOptAtomType:implIPubJavaOptAtomType:g
+1,$s:implIJavaReqAtomType:implIPubJavaReqAtomType:g
+w
+q
+
