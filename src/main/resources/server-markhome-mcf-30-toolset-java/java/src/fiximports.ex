0543afdb061ff975b5ff3435f750f14bc1c7b80f
Mark Sobkow <mark.sobkow@gmail.com>
2026-08-28 04:43:18 -0600

    2026-08-28 04:45 CST The fine point details are being worked on now, unfortunately including some unresolved rule references that I can see definitions for in the knowledge base, and they're pretty simple overall, so I don't understand why they aren't showing up. Clearly I am missing something somewhere, but I won't bang away at it any more right now - time for some sleep.

--- src/main/resources/server-markhome-mcf-30-toolset-java/java/src/fiximports.ex
+++
@@ -1,4 +0,0 @@
-1,$s:^\(.*mport.*\)\.dbutil:\1.dbutil\\1.keyhash:g
-w
-q
-
--- src/main/resources/server-markhome-mcf-30-toolset-java/java/src/fixpubtypes.ex
+++
@@ -1,6 +0,0 @@
-1,$s:implIJavaAtomType:implIPubJavaAtomType:g
-1,$s:implIJavaOptAtomType:implIPubJavaOptAtomType:g
-1,$s:implIJavaReqAtomType:implIPubJavaReqAtomType:g
-w
-q
-
